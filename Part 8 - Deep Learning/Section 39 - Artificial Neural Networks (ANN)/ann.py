a040f30e11c82c59da7972c391a079148ea54aec
Thanee Ananitthi <sawatdeekrab@gmail.com>
2017-05-10 12:25:50 -0700

    Add Grid Search to ANN

--- Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann.py
+++ Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann.py
@@ -89,6 +89,8 @@ cm = confusion_matrix(y_test, y_pred)
 #Evaluating ANN
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
+from keras.models import Sequential
+from keras.layers import Dense
 def build_classifier():
     classifier = Sequential()
     classifier.add(Dense(units=6,kernel_initializer='uniform',activation='relu',input_dim=11))
@@ -107,6 +109,43 @@ variance = accuracies.std()
 #Dropout Regularrizaion to Reduce overfitting if it needed
 
 #Tuning ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+def build_classifier(localOptimizer):
+    classifier = Sequential()
+    classifier.add(Dense(units=6,kernel_initializer='uniform',activation='relu',input_dim=11))
+    classifier.add(Dense(units=6,kernel_initializer='uniform',activation='relu'))
+    classifier.add(Dense(units=1,kernel_initializer='uniform',activation='sigmoid'))
+    classifier.compile(optimizer= localOptimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn = build_classifier)
+parameters = {'batch_size' : [25, 32],
+              'epochs' : [100, 500],
+              'localOptimizer' : ['adam', 'rmsprop']}
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10)
+grid_search = grid_search.fit(X_train, y_train)
+best_parameters = grid_search.best_params_
+beat_accuracy = grid_search.best_score_
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
